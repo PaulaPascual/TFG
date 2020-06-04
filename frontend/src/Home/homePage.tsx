83a250b4c0315f59d6bde802861114059863d4c2
Paula <paula.pascual@live.u-tad.com>
2020-06-04 12:16:09 +0200

    soy mogui y he puesto home en welcome, arreglado. Boton redirect

--- frontend/src/Home/homePage.tsx
+++ frontend/src/Home/homePage.tsx
@@ -5,16 +5,7 @@ import {Grid, List, Box} from "@material-ui/core";
 
 const HomePage = () => {
     return (
-        <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item xs={6} container justify="center" alignItems="center" style={{"maxHeight": "100%"}}>
-                <img src={"/static/images/tesla.jpg"} style={{"maxWidth": "60%"}}/>
-            </Grid>
-            <Grid item xs={6} container direction="column" justify="center" alignItems="center" style={{"maxHeight": "100%", "color": "#00f9f7"}}>
-                <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
-                que tal
-                <Button variant="contained" size="small" color="primary" className="theme">Empezar</Button>
-            </Grid>
-        </Grid>
+        <div style={{"color": "#ffffff"}}>Home</div>
     );
 };
 
--- frontend/src/Welcome/welcomePage.tsx
+++ frontend/src/Welcome/welcomePage.tsx
@@ -1,11 +1,28 @@
-import {AppBar, Toolbar} from "@material-ui/core";
+import {AppBar, Button, Grid, Toolbar} from "@material-ui/core";
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
+import { Redirect } from 'react-router-dom';
 
-const WelcomePage = () => {
+export default function WelcomePage() {
+    const [isReturn, setReturn] = useState(false)
+
+    const goHome=()=>{
+          setReturn(true);
+    }
+    if (isReturn){
+        return <Redirect to='/home'/>
+    }
+
     return (
-        <div style={{"color": "#ffffff"}}>Hola soy el welcome</div>
+        <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs={6} container justify="center" alignItems="center" style={{"maxHeight": "100%"}}>
+                <img src={"/static/images/tesla.jpg"} style={{"maxWidth": "60%"}}/>
+            </Grid>
+            <Grid item xs={6} container direction="column" justify="center" alignItems="center" style={{"maxHeight": "100%", "color": "#00f9f7"}}>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
+                que tal
+                <Button variant="contained" color="primary" onClick={goHome}>Empezar</Button>
+            </Grid>
+        </Grid>
     );
-};
-
-export default WelcomePage;
+}
