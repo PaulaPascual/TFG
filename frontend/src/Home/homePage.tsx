fccbaf8125eedc6e5bc06b8ea91e6a61e939fcc9
Paula <paula.pascual@live.u-tad.com>
2020-06-04 12:26:27 +0200

    home page mockeada

--- frontend/src/Home/homePage.tsx
+++ frontend/src/Home/homePage.tsx
@@ -5,7 +5,26 @@ import {Grid, List, Box} from "@material-ui/core";
 
 const HomePage = () => {
     return (
-        <div style={{"color": "#ffffff"}}>Home</div>
+        <Grid container direction="row" justify="center" alignItems="center">
+             <Grid item xs={4} container direction="column" justify="center" alignItems="center" style={{"maxHeight": "100%", "color": "#00f9f7"}}>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
+            </Grid>
+            <Grid item xs={4} container justify="center" alignItems="center" style={{"maxHeight": "100%"}}>
+                <img src={"/static/images/tesla.jpg"} style={{"maxWidth": "60%"}}/>
+            </Grid>
+           <Grid item xs={4} container direction="row" justify="center" alignItems="center" style={{"maxHeight": "100%", "color": "#00f9f7"}}>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+                <img src={"/static/images/hola.png"} style={{"maxWidth": "30%"}}/>
+
+
+            </Grid>
+        </Grid>
     );
 };
 
