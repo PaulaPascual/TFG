5bb1dfbec593c6c4376de3774b7065cfef83fb71
Paula <paula.pascual@live.u-tad.com>
2020-06-10 19:09:24 +0200

    welcome modificado

--- frontend/src/Welcome/welcomePage.tsx
+++ frontend/src/Welcome/welcomePage.tsx
@@ -1,19 +1,15 @@
 import {AppBar, Button, Grid, Toolbar} from "@material-ui/core";
 import {Link} from "react-router-dom";
-import React, {useState} from "react";
+import React, {useState, Component} from "react";
 import { Redirect } from 'react-router-dom';
 
-export default function WelcomePage() {
-    const [isReturn, setReturn] = useState(false)
+class WelcomePage extends Component<any, any> {
 
-    const goHome=()=>{
-          setReturn(true);
-    }
-    if (isReturn){
-        return <Redirect to='/home'/>
-    }
-
-    return (
+    goHome =()=>{
+        this.props.history.push('/home')
+    };
+    render(){
+        return (
         <Grid container direction="row" justify="center" alignItems="center" style={{"marginTop":"5vh"}}>
             <Grid item xs={12} sm={6}container justify="center" alignItems="center" style={{"maxHeight": "100%"}}>
                 <img src={"/static/images/tesla.jpg"} style={{"maxWidth": "60%"}}/>
@@ -21,8 +17,11 @@ export default function WelcomePage() {
             <Grid item xs={12} sm={6} container direction="column" justify="center" alignItems="center" style={{"maxHeight": "100%", "color": "#00f9f7"}}>
                 <img src={"/static/images/hola.png"} style={{"maxWidth": "60%"}}/>
                 que tal
-                <Button variant="contained" color="primary" onClick={goHome}>Empezar</Button>
+                <Button variant="contained" color="primary" onClick={this.goHome}>Empezar</Button>
             </Grid>
         </Grid>
-    );
-}
+        );
+    }
+
+} export default WelcomePage;
+
